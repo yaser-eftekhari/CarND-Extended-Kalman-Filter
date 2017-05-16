43eb25a520c3ccedd1b6bcca8c871f1c3e891f1c
yaser-eftekhari <yaser.eftekhari@gmail.com>
2017-05-15 20:16:38 -0400

    modified gitignore to remove all CMake generated filed. Added std to tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -19,7 +19,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//  * the estimation vector size should equal ground truth vector size
 	if(estimations.size() != ground_truth.size()
 			|| estimations.size() == 0){
-		cout << "Invalid estimation or ground_truth data" << endl;
+		std::cout << "Invalid estimation or ground_truth data" << std::endl;
 		return rmse;
 	}
 
@@ -60,7 +60,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
 	//check division by zero
 	if(fabs(c1) < 0.0001){
-		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+		std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
 		return Hj;
 	}
 
